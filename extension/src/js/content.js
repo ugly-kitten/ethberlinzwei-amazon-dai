0b077c728154537657ae00a4ade29a3dab5416ca
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2019-08-24 17:12:50 +0200

    added pay with dai button

--- extension/src/js/content.js
+++ extension/src/js/content.js
@@ -1,6 +1,47 @@
-document.body.style.background = 'green';
+let payAction = () => {
+    document.getElementById("gcpromoinput").value = "dai dai";
+}
 
-var iFrame  = document.createElement ("iframe");
-iFrame.src  = chrome.extension.getURL ("embedded.html");
+let wrapperForDai = document.getElementById("imb-wrapper");
+let daiBox;
 
-document.body.insertBefore (iFrame, document.body.firstChild);
+let addBox = () => {
+    daiBox = document.createElement('div');
+    daiBox.setAttribute('class', 'a-box a-spacing-base');
+    wrapperForDai.appendChild(daiBox);
+
+};
+
+let addHeadline = () => {
+    var headline = document.createElement('h1');
+    headline.setAttribute("class", "a-spacing-base");
+    headline.innerHTML = "Pay with Crypto";
+    wrapperForDai.appendChild(headline);
+};
+
+let addDAIButton = () => {
+    let button = document.createElement('input');
+    button.setAttribute('type', 'button');
+    button.setAttribute('value', 'Pay with DAI');
+    button.setAttribute('class', ' a-button-text');
+    button.addEventListener("click", payAction, false);
+    wrapperForDai.appendChild(document.createElement('br'));
+
+    let text = document.createElement('p');
+    text.value = "At Amazon we support your Crypto";
+
+   daiBox.appendChild(text);
+   daiBox.appendChild(button);
+
+
+};
+addHeadline();
+addBox();
+addDAIButton();
+
+
+//
+// var iFrame  = document.createElement ("iframe");
+// iFrame.src  = chrome.extension.getURL ("embedded.html");
+//
+// document.body.insertBefore (iFrame, document.body.firstChild);
--- extension/src/js/main.js
+++ extension/src/js/main.js
@@ -4,4 +4,6 @@ console.log("fuu");
 function jQueryMain () {
     $("body").append ('<p>Added by jQuery</p>');
     console.log("test test");
+
+    $("#gcpromoinput").val("holasoyunvoucher");
 }
