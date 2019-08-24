ab8caa97e9d4cee2d7d38411730cdd02d791cdfe
Miguel Hervas <miguel.hervas.lazaro@gmail.com>
2019-08-24 14:25:42 +0200

    wip

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,16 +1,24 @@
 import Router from "koa-router"
+var bodyParser = require("koa-bodyparser")
 
 import { Config } from "./config"
-import { sayHello } from "./routes/hello"
+import { processVoucherRequest } from "./routes/voucher"
 
 export default function createRouter(config: Config) {
   const router = new Router()
-
+  router.use(bodyParser())
+  router.use(async ctx => {
+    // the parsed body will store in ctx.request.body
+    // if nothing was parsed, body will be an empty object {}
+    ctx.body = ctx.request.body
+  })
   router.prefix(config.basePath)
 
-  router.get("/hello/:name", function(ctx) {
-    console.log("context params", ctx.params.name)
-    ctx.body = sayHello(ctx.params.name)
+  router.post("/voucher", function(ctx) {
+    console.log("Request Context", ctx)
+    ctx.body = processVoucherRequest(ctx.body)
+    ctx.status = 200
+    console.log("Response Context", ctx.response)
   })
 
   return router
